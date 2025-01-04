519f126795f07c74c68e7bfafb2f9c1d6f0363b0
Joa365 <febra_fe@wp.pl>
2025-01-04 23:55:38 +0100

    day 5 part 1 correct

--- AdventOfcode/Day5/Day5Part1.cs
+++ AdventOfcode/Day5/Day5Part1.cs
@@ -5,11 +5,11 @@ namespace Day5
     {
         string line;
         List<string> codePuzzle = new List<string>();
-        List<(int, int)> pageOrderingRules = new List<(int, int)>();
-        List<string> safetyManual = new List<string>();
+        // List<(int, int)> pageOrderingRules = new List<(int, int)>();
 
-        // string csvFile = "Day5/test4.csv";
-        string csvFile = "testData.csv";
+
+        string csvFile = "Day5/input5.csv";
+        // string csvFile = "testData.csv";
 
         public void RunDay5()
         {
@@ -27,7 +27,7 @@ namespace Day5
             while (line != null)
             {
                 //write the line to console window
-                Console.WriteLine(line);
+                // Console.WriteLine(line);
                 codePuzzle.Add(line);
                 //Read the next line
                 line = sr.ReadLine();
@@ -39,14 +39,17 @@ namespace Day5
         private void SeparateRulesFromManual(List<string> puzzles)
         {
             int emptyPuzzle = 0;
-            
+            int puzzlesNumber = puzzles.Count();
 
-            for(int i = 0; i < puzzles.Count(); i++)
+            List<(string, string)> pageOrderingRules = new List<(string, string)>();
+            List<string> safetyManual = new List<string>();
+            
+            for(int i = 0; i < puzzlesNumber; i++)
             {
                 if(!string.IsNullOrEmpty(puzzles[i]))
                 {
-                    // string[] slpitedPuzzle = puzzles[i].Split('|');
-                    int[] slpitedPuzzle = puzzles[i].Split('|').Select(x => int.Parse(x)).ToArray();
+                    string[] slpitedPuzzle = puzzles[i].Split('|');
+                    // int[] slpitedPuzzle = puzzles[i].Split('|').Select(x => int.Parse(x)).ToArray();
 
                     // pageOrderingRules.Add((int.Parse(slpitedPuzzle[0]), int.Parse(slpitedPuzzle[1])));
                     pageOrderingRules.Add((slpitedPuzzle[0], slpitedPuzzle[1]));
@@ -56,11 +59,67 @@ namespace Day5
                 else
                 {
                     emptyPuzzle = i;
-                    Console.WriteLine($"{emptyPuzzle}");
+                    // Console.WriteLine($"{emptyPuzzle}");
                     break;
                 }
             }
+
+            for( int i = emptyPuzzle + 1; i < puzzlesNumber; i ++)
+            {
+                // Console.WriteLine($"{puzzles[i]}");
+                safetyManual.Add(puzzles[i]);
+            }
+
+            SelectPagesInCorrectOrder(pageOrderingRules, safetyManual);
         }
 
+        private void SelectPagesInCorrectOrder(List<(string pageOne, string pageTwo)> manuals, List<string> pagesToPrint)
+        {
+            List<string> correctInOrderPages = new List<string>();
+            int maualLenght = manuals.Count();
+
+            foreach(string page in pagesToPrint)
+            {
+                int i = 0;
+
+                foreach(var manual in manuals)
+                {
+                    int indexPageOne = page.IndexOf(manual.pageOne);
+                    int indexPageTwo = page.IndexOf(manual.pageTwo);
+
+                    if(indexPageTwo != -1 && indexPageTwo !=-1)
+                    {
+                        if(indexPageOne > indexPageTwo)
+                        {
+                            break;
+                        }
+                    }
+                    i++;
+                }
+
+                if(i == maualLenght)
+                {
+                    correctInOrderPages.Add(page);
+                    // Console.WriteLine($"{page}");
+                }
+            }
+
+            // Console.WriteLine($"{correctInOrderPages.Count()}");
+            MiddelValue(correctInOrderPages);
+        }
+
+        private void MiddelValue(List<string> correctPages)
+        {
+            int sum = 0;
+            foreach(var page in correctPages)
+            {
+                int[] splitedPages = page.Split(',').Select(x => int.Parse(x)).ToArray(); 
+                int amountSplitedPages = splitedPages.Count();
+                int middleIndex = (amountSplitedPages - 1) / 2;
+                sum += splitedPages[middleIndex];
+            }
+
+            Console.WriteLine($"Wynik obliczen: {sum}");
+        }
     }
 }
--- AdventOfcode/obj/Debug/net8.0/AdventOfcode.AssemblyInfo.cs
+++ AdventOfcode/obj/Debug/net8.0/AdventOfcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7396bedf7f4feed2cff07c6e29df13238a04117a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21921613293356b3edcda77c65868cf5619fb1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
